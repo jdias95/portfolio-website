5103e13bde8c921f5d136592e219176a8dbf5e3c
AkomaMathaino <dias.joshua7@yahoo.com>
2022-01-19 13:32:16 -0800

    test

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
     include_once("home.html");
     echo htmlspecialchars($_SERVER["PHP_SELF"]);
 
-    // $nameErr = $emailErr = $messageErr = "";
-    // $name = $email = $message = $to = $subject = $body "";
+    $nameErr = $emailErr = $messageErr = "";
+    $name = $email = $message = $to = $subject = $body "";
 
-    // function test_input($data) {
-    //     $data = trim($data);
-    //     $data = stripslashes($data);
-    //     $data = htmlspecialchars($data);
-    //     return $data;
-    // }
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 
     // if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //     if (empty($_POST["name"])) {

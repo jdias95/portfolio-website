b8f957bff81c79ee0782dab549133f74647ca2d1
AkomaMathaino <dias.joshua7@yahoo.com>
2022-01-19 12:51:26 -0800

    test

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
-    include_once("home.html");
-    
+    echo "hello";
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $from = 'From: Website'; 
     $to = 'dias.joshua7@yahoo.com'; 
     $subject = 'New Form Submission';
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent!</p>';
-    } else { 
-        echo '<p>Something went wrong, go back and try again!</p>'; 
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $body)) { 
+            echo '<p>Your message has been sent!</p>';
+        } else { 
+            echo '<p>Something went wrong, go back and try again!</p>'; 
+        }
     }
-}
+
+    include_once("home.html");
 ?>

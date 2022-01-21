06c0a08aa7833320b1b3a157f98da889be1fa221
AkomaMathaino <dias.joshua7@yahoo.com>
2022-01-21 15:04:44 -0800

    test

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-    echo "Hello";
-    include_once("home.html");
+    
     // echo htmlspecialchars($_SERVER["PHP_SELF"]);
 
-    // $nameErr = $emailErr = $messageErr = $name = $email = $message = $to = $subject = $body "hello";
-    
+    $nameErr = $emailErr = $messageErr = $name = $email = $message = $to = $subject = $body "hello";
+    echo $name;
+    include_once("home.html");
 
     // function test_input($data) {
     //     $data = trim($data);

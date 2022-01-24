eb27f4f757a66afbcffeba8af9321c5f9c99b817
AkomaMathaino <dias.joshua7@yahoo.com>
2022-01-24 12:26:36 -0800

    test

--- index.php
+++ index.php
@@ -1,49 +1,48 @@
 <?php
     
-    echo htmlspecialchars($_SERVER["PHP_SELF"]);
+    // echo htmlspecialchars($_SERVER["PHP_SELF"]);
 
-    $nameErr = $emailErr = $messageErr = $name = $email = $message = $to = $subject = $body = "";
-    echo $name;
+    // $nameErr = $emailErr = $messageErr = $name = $email = $message = $to = $subject = $body = "";
     include_once("home.html");
 
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    };
+    // function test_input($data) {
+    //     $data = trim($data);
+    //     $data = stripslashes($data);
+    //     $data = htmlspecialchars($data);
+    //     return $data;
+    // };
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        echo "hello";
-        mail($to, $subject, $body);
-        if (empty($_POST["name"])) {
-            $nameErr = "Name is required";
-        } else {
-            $name = test_input($_POST["name"]);
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                $nameErr = "Only letters and white space allowed";
-            };
-        };
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     echo "hello";
+    //     mail($to, $subject, $body);
+    //     if (empty($_POST["name"])) {
+    //         $nameErr = "Name is required";
+    //     } else {
+    //         $name = test_input($_POST["name"]);
+    //         if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+    //             $nameErr = "Only letters and white space allowed";
+    //         };
+    //     };
         
-        if (empty($_POST["email"])) {
-            $emailErr = "Email is required";
-        } else {
-            $email = test_input($_POST["email"]);
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format";
-            };
-        };
+    //     if (empty($_POST["email"])) {
+    //         $emailErr = "Email is required";
+    //     } else {
+    //         $email = test_input($_POST["email"]);
+    //         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //             $emailErr = "Invalid email format";
+    //         };
+    //     };
         
-        if (empty($_POST["message"])) {
-            $messageErr = "Message is required";
-        } else {
-            $message = test_input($_POST["message"]);
-        };
-        $to = 'dias.joshua7@yahoo.com';
-        $subject = 'New Form Submission';
-        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-        mail($to, $subject, $body);
-    };
+    //     if (empty($_POST["message"])) {
+    //         $messageErr = "Message is required";
+    //     } else {
+    //         $message = test_input($_POST["message"]);
+    //     };
+    //     $to = 'dias.joshua7@yahoo.com';
+    //     $subject = 'New Form Submission';
+    //     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+    //     mail($to, $subject, $body);
+    // };
     
     $name = $_POST['name'];
     $email = $_POST['email'];
@@ -54,6 +53,7 @@
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
     if ($_POST['submit']) {
+        echo $name;
         if (@mail($to, $subject, $body)) { 
             echo '<p>Your message has been sent!</p>';
         } else { 

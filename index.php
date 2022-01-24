ff205d41b4f7580b47d716388f17af3670c47428
AkomaMathaino <dias.joshua7@yahoo.com>
2022-01-24 12:16:24 -0800

    test

--- index.php
+++ index.php
@@ -45,19 +45,19 @@
         mail($to, $subject, $body);
     };
     
-    // $name = $_POST['name'];
-    // $email = $_POST['email'];
-    // $message = $_POST['message'];
-    // $to = 'dias.joshua7@yahoo.com'; 
-    // $subject = 'New Form Submission';
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'dias.joshua7@yahoo.com'; 
+    $subject = 'New Form Submission';
 
-    // $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-    // if ($_POST['submit']) {
-    //     if (mail ($to, $subject, $body)) { 
-    //         echo '<p>Your message has been sent!</p>';
-    //     } else { 
-    //         echo '<p>Something went wrong, go back and try again!</p>'; 
-    //     }
-    // }
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $body)) { 
+            echo '<p>Your message has been sent!</p>';
+        } else { 
+            echo '<p>Something went wrong, go back and try again!</p>'; 
+        };
+    };
 ?>
